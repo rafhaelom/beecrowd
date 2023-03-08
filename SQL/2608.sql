023934071a53a8272b8027727ff76272ef0d79f5
Rafhael Martins <rafhael.martins@vert.com.br>
2023-03-08 17:38:39 -0300

    :sparkles: add: Resolução desafios.

---
+++ SQL/2608.sql
@@ -0,0 +1,28 @@
+--- Beecrowd Problem 2608 - Maior e Menor Preço ---
+
+--- Tabela ---
+CREATE TABLE products (
+  id numeric PRIMARY KEY,
+  name varchar(50),
+  amount numeric,
+  price numeric(7,2)
+);
+
+--- Dados ---
+INSERT INTO products (id, name, amount, price)
+VALUES
+  (1,	'Two-doors wardrobe',	100,	800),
+  (2,	'Dining table',	1000,	560),
+  (3,	'Towel holder',	10000,	25.50),
+  (4,	'Computer desk',	350,	320.50),
+  (5,	'Chair',	3000,	210.64),
+  (6,	'Single bed',	750,	460);
+  
+/*  Execute this query to drop the tables */
+-- DROP TABLE products; --
+
+--- Resolução ---
+select 
+	max(price) as price,
+	min(price) as price
+from products;  

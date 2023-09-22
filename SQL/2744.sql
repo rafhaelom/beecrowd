9717583f81166b4497e6887c2e30277919ebde8e
Rafhael Martins <rafhael.martins@vert.com.br>
2023-09-22 17:35:14 -0300

    :sparkles: add: Resolução desafios.

---
+++ SQL/2744.sql
@@ -0,0 +1,27 @@
+---- Beecrowd Problem 2744 - Senhas ---
+
+CREATE TABLE account(
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(255),
+  login VARCHAR(255),
+  password VARCHAR(255)
+  );
+  
+  
+INSERT INTO account(id, name, login, password)
+VALUES (1, 'Joyce P. Parry', 'Promeraw', 'noh1Oozei'),
+       (2, 'Michael T. Gonzalez', 'Phers1942', 'Iath3see9bi'),
+       (3, 'Heather W. Lawless', 'Hankicht', 'diShono4'),
+       (4, 'Otis C. Hitt', 'Conalothe', 'zooFohH7w'),
+       (5, 'Roger N. Brownfield', 'Worseente', 'fah7ohNg');
+
+  
+  /*  Execute this query to drop the tables */
+  -- DROP TABLE account; --
+
+--- Resolução ---
+select 
+	id, 
+	password, 
+	MD5(password) 
+from account;
